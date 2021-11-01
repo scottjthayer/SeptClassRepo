457aa7551cb68f6e45bda55285d7328a18b52fe2
Jeo <jeo961@gmail.com>
2021-11-01 09:58:07 -0400

    coffee

---
+++ Unit 5 ASP.Net/CoffeeShopModel/CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Results(User U)
+        {
+            if (U.Password.ToLower() == "password")
+            {
+                //ViewData["ErrorMsg"] = "Please enter a different password";
+                return RedirectToAction("Registration");
+            }
+            
+            return View(U);
+        }
+
+        //public IActionResult Results(string First, string Last, string Email, string Password)
+        //{
+        //    if(Password.ToLower() == "password")
+        //    {
+        //        //ViewData["ErrorMsg"] = "Please enter a different password";
+        //        return RedirectToAction("Registration");
+        //    }
+        //    ViewData["First"] = First;
+        //    ViewData["Last"] = Last;
+        //    ViewData["Email"] = Email;
+        //    return View();
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unit 5 ASP.Net/CoffeeShopModel/CoffeeShop/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class User
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //No Constructors. Breaks code
+        //public User(string first, string last, string email, string password)
+        //{
+        //    First = first;
+        //    Last = last;
+        //    Email = email;
+        //    Password = password;
+        //}
+    }
+}
