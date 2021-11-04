957ec00961c8d51be5de13b8b10d5a15ce1bb3f8
Josh <thejosh2021@gmailcom>
2021-11-03 21:28:19 -0400

    removed html client

--- Unit 5 ASP.Net/CardDemo/CardDemo/Controllers/HomeController.cs
+++ Unit 5 ASP.Net/CardDemo/CardDemo/Controllers/HomeController.cs
@@ -15,46 +15,39 @@ namespace CardDemo.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private CardDAL cardDAL = new CardDAL();
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://www.deckofcardsapi.com/");
-            var response = await client.GetAsync("/api/deck/new/shuffle/?deck_count=1");
-            // string result = await response.Content.ReadAsStringAsync();
-            // ViewBag.result = result;
-            Deck cards = await response.Content.ReadAsAsync<Deck>();
-
-            response = await client.GetAsync($"/api/deck/{cards.deck_id}/draw/?count=5");
-            CardSet cset = await response.Content.ReadAsAsync<CardSet>();
+            
+            Deck cards = cardDAL.GetDeck();
+            cardDAL.ShuffleDeck(cards.deck_id);
+            CardSet cset = cardDAL.GetCards(cards.deck_id,0);
 
             return View(cset);
         }
         [HttpPost]
-        public async Task<IActionResult> Index(string deckId, List<int> keepIndex, List<Card> hand)
+        public IActionResult Index(string deckId, List<int> keepIndex, List<Card> hand)
         {
-            List<Card> keepCard = new List<Card>();
+            List<Card> keepCards = new List<Card>();
 
             foreach(int i in keepIndex)
             {
-                keepCard.Add(hand[i]);
+                keepCards.Add(hand[i]);
             }
 
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://www.deckofcardsapi.com/");
-
             // only need to draw for each card you discarded
-            var response = await client.GetAsync($"/api/deck/{deckId}/draw/?count={5- keepCard.Count}");
-            CardSet cset = await response.Content.ReadAsAsync<CardSet>();
+            CardSet cset = cardDAL.GetCards(deckId,keepCards.Count);
 
             // Add the old hand back
-            if(keepCard != null)
+            if (keepCards != null)
             {
-                cset.cards.InsertRange(0,keepCard);
+                cset.cards.InsertRange(0,keepCards);
             }
 
             return View(cset);
---
+++ Unit 5 ASP.Net/CardDemo/CardDemo/Models/CardDAL.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CardDemo.Models
+{
+    public class CardDAL
+    {
+        public Deck GetDeck()
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/new/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            Deck result = JsonConvert.DeserializeObject<Deck>(JSON);
+
+            return result;
+        }
+
+        public void ShuffleDeck(string id)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{id}/shuffle/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+
+        public CardSet GetCards(string id, int keepCards)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{id}/draw/?count={5-keepCards}";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            CardSet result = JsonConvert.DeserializeObject<CardSet>(JSON);
+
+            return result;
+        }
+    }
+}
